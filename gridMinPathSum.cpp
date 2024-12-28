3d4b107599700f9c31b1fe17fa9817933d4f59c2
HarshavrdhanSinghParwal <96642006+Harsh-Parwal@users.noreply.github.com>
2024-12-28 13:29:08 +0530

    Add files via upload

---
+++ gridMinPathSum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int minPath(vector<vector<int>>& grid,int m,int n,vector<vector<int>>& dp){
+    if(m==0 && n==0){
+        return grid[0][0];
+    }
+    if(m<0 || n<0){
+        return 1e9;
+    }
+    if(dp[m][n]!=-1){
+        return dp[m][n];
+    }
+    int up=grid[m][n]+minPath(grid,m-1,n,dp);
+    int left=grid[m][n]+minPath(grid,m,n-1,dp);
+    return dp[m][n]=min(up,left);
+}
+int minPathSum(vector<vector<int>>& grid){
+    int m=grid.size();
+    int n=grid[0].size();
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    return minPath(grid,m-1,n-1,dp); 
+}
+int main(){
+
+}
