bfa1fcb3e478612036a0f72e917737a190a43b8c
HarshavrdhanSinghParwal <96642006+Harsh-Parwal@users.noreply.github.com>
2025-07-12 17:13:42 +0530

    https://leetcode.com/problems/house-robber/

--- houseRobber.cpp
+++ houseRobber.cpp
@@ -1,21 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-// https://leetcode.com/problems/house-robber/description/
-int maxLoot(vector<int>&nums ,int idx,vector<int>& dp){
-    if(idx>=nums.size()){
-        return 0;
+    #include<bits/stdc++.h>
+    using namespace std;
+    // https://leetcode.com/problems/house-robber/
+    int robMoney(vector<int>& nums,int idx,vector<int>&dp){
+        if(idx>=nums.size()){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int notake=robMoney(nums,idx+1,dp);
+        int take=nums[idx]+robMoney(nums,idx+2,dp);
+        return dp[idx]=max(notake,take);
     }
-    if(dp[idx]!=-1){
-        return dp[idx];
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return robMoney(nums,0,dp);
     }
-    int notake=maxLoot(nums,idx+1,dp);
-    int take=nums[idx]+maxLoot(nums,idx+2,dp);
-    return dp[idx]=max(notake,take);
-}
-int rob(vector<int>&nums){
-    vector<int> dp(nums.size(),-1);
-    return maxLoot(nums,0,dp);
-}
-int main(){
+    int main(){
 
-}
+    }
